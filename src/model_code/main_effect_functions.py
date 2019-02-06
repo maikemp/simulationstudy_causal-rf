87b57134961dd1663f6213a7996331540308ec9e
Maike Metz-Peeters <maike-m-p@web.de>
2019-02-06 12:15:28 +0100

    Added files for the specification of the fucntions describing the data generating process.

---
+++ src/model_code/main_effect_functions.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+def main_effect_1(x):
+    main_effect = 2 * (x[1] - 0.5)
+    return main_effect
---
+++ src/model_code/propensity_functions.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+def propensity_1(x):
+    propensity = 0.5
+    return propensity
