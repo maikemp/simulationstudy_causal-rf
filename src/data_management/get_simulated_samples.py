efc6d77526b839ad62be23f2a07ed3cb4d8681b6
Maike Metz-Peeters <maike-m-p@web.de>
2019-02-07 13:29:03 +0100

    First attempt to implement double loop in the waf scripts.

--- src/data_management/get_simulated_samples.py
+++ src/data_management/get_simulated_samples.py
@@ -1,7 +1,8 @@
 """Draw simulated dataset using model specifications specified in
 IN_MODEL_SPECS and store in a numpy array.
 
-...(explain more precisely what is happening here)...
+Module requires to be given a setup corresponding to a json file and a 
+repetition number indicating the repetition currently at for the simulation. 
 
 """
 
@@ -11,58 +12,53 @@ import sys
 import json
 import numpy as np
 import pandas as pd
-import pickle
 from bld.project_paths import project_paths_join as ppj
 
-import src.model_code.main_effect_functions
-import src.model_code.treatment_effect_functions
-import src.model_code.propensity_functions
+import src.model_code.main_effect_functions as mef
+import src.model_code.treatment_effect_functions as tef
+import src.model_code.propensity_functions as pf
 
-##später wieder löschen: !!
-path = '/Users/maike-mp/UniBonn/5.Semester/MasterThesis/simulationstudy_ci_causal_rf/src/model_code/propensity_functions.py'
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.expanduser(path)))
-import propensity_functions as pf
-import treatment_effect_functions as tef
-import main_effect_functions as mef
-
-model = json.load(
-    open('/Users/maike-mp/UniBonn/5.Semester/MasterThesis/simulationstudy_ci_causal_rf/src/model_specs/setup_1.json'), encoding="utf-8"
-)
-
-model = json.load(open(ppj("IN_MODEL_SPECS", setup + ".json"), encoding="utf-8"))
 
 def get_simulated_sample(setup):
     """Simulate a sample ..."""
     
-    if model['x_distr'] == 'normal':
-        X = np.random.multivariate_normal(model['x_mean'], model['x_cov'], model['n'])     
-    if model['x_distr'] == 'uniform':
-        X = np.random.uniform(model['x_low'], model['x_high'],(model['n'],model['d']))
+    if setup['x_distr'] == 'normal':
+        X = np.random.multivariate_normal(setup['x_mean'], setup['x_cov'], setup['n'])     
+    if setup['x_distr'] == 'uniform':
+        X = np.random.uniform(setup['x_low'], setup['x_high'],(setup['n'],setup['d']))
         
-    prop_function = getattr(pf, model['propensity_function'])
+    prop_function = getattr(pf, setup['propensity_function'])
     propensity = np.apply_along_axis(prop_function, 1, X)
     
-    W = np.random.binomial(1, propensity, model['n'])
+    W = np.random.binomial(1, propensity, setup['n'])
     
-    treat_function = getattr(tef, model['treatment_effect_function'])
+    treat_function = getattr(tef, setup['treatment_effect_function'])
     true_treat_effect = np.apply_along_axis(treat_function, 1, X)
     
-    main_function = getattr(mef, model['main_effect_function'])
-    epsilon = np.random.normal(0, model['sigma'], model['n'])
+    main_function = getattr(mef, setup['main_effect_function'])
+    epsilon = np.random.normal(0, setup['sigma'], setup['n'])
     Y = np.apply_along_axis(main_function, 1, X) + W * true_treat_effect + epsilon
     
     stack = np.column_stack((X, W, Y, true_treat_effect))
     
     columns = list()
-    for i in range(model['d']):
+    for i in range(setup['d']):
         columns.append('X_{}'.format(i))
     columns.extend(['W', 'Y', 'true_te'])
     
     data = pd.DataFrame(stack, columns=columns)
     return data
+
  
-test = get_simulated_sample(model)
- 
+def save_data(sample, setup_name, rep_number):
+    sample.to_pickle(ppj("OUT_DATA", 'sample_{}_rep_{}'.format(setup_name, rep_number)))    
+
+if __name__ == "__main__":
+    setup_name = sys.argv[1]
+    rep_number = sys.argv[2]
+    setup = json.load(open(ppj("IN_MODEL_SPECS", setup_name + ".json"), encoding="utf-8"))
+    
+    data = get_simulated_sample(setup)
+    save_data(data, setup, rep_number)
+
 
--- src/model_code/treatment_effect_functions.py
+++ src/model_code/treatment_effect_functions.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-def treatment_factor_1(x):
+def _treatment_factor_1(x):
     factor = 2/(1+np.exp(-12*(x-0.5)))
     return factor
 
-def treatment_effect_function_1(X):
-    treatment_effect = treatment_factor_1(X[0])*treatment_factor_1(X[1])
+def treatment_effect_function_1(X, q):
+    treatment_effect = _treatment_factor_1(X[0])*_treatment_factor_1(X[1])
     return treatment_effect
