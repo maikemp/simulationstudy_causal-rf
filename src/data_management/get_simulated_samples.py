01e6448de34d221e2a7224002206b5ca33816f7a
Maike Metz-Peeters <maike-m-p@web.de>
2019-02-08 10:17:05 +0100

    Added a loop over different sample sizes as a simulation parameter, not specific to one setup.

--- src/data_management/get_simulated_samples.py
+++ src/data_management/get_simulated_samples.py
@@ -18,24 +18,24 @@ import src.model_code.main_effect_functions as mef
 import src.model_code.treatment_effect_functions as tef
 import src.model_code.propensity_functions as pf
 
-def get_simulated_sample(setup):
+def get_simulated_sample(setup, n):
     """Simulate a sample ..."""
     
     if setup['x_distr'] == 'normal':
-        X = np.random.multivariate_normal(setup['x_mean'], setup['x_cov'], setup['n'])     
+        X = np.random.multivariate_normal(setup['x_mean'], setup['x_cov'], n)     
     if setup['x_distr'] == 'uniform':
-        X = np.random.uniform(setup['x_low'], setup['x_high'],(setup['n'],setup['d']))
+        X = np.random.uniform(setup['x_low'], setup['x_high'],(n, setup['d']))
         
     prop_function = getattr(pf, setup['propensity_function'])
     propensity = np.apply_along_axis(prop_function, 1, X)
     
-    W = np.random.binomial(1, propensity, setup['n'])
+    W = np.random.binomial(1, propensity, n)
     
     treat_function = getattr(tef, setup['treatment_effect_function'])
     true_treat_effect = np.apply_along_axis(treat_function, 1, X)
     
     main_function = getattr(mef, setup['main_effect_function'])
-    epsilon = np.random.normal(0, setup['sigma'], setup['n'])
+    epsilon = np.random.normal(0, setup['sigma'], n)
     Y = np.apply_along_axis(main_function, 1, X) + W * true_treat_effect + epsilon
     
     stack = np.column_stack((X, W, Y, true_treat_effect))
@@ -52,16 +52,18 @@ def get_simulated_sample(setup):
     
 if __name__ == "__main__":
     setup_name = sys.argv[1]
-    rep_number = sys.argv[2]
+    n = sys.argv[2]
+    rep_number = sys.argv[3]
     
     setup = json.load(open(ppj("IN_MODEL_SPECS", setup_name + ".json"), encoding="utf-8"))
+    sim_param = json.load(open(ppj("IN_MODEL_SPECS", "simulation_parameters.json"), encoding="utf-8"))
 
 
     # Load initial locations and setup agents
-    data = get_simulated_sample(setup)
+    data = get_simulated_sample(setup, int(n))
     # Run the main analysis
     #locations_by_round = run_analysis(agents, setup)
     # Store list with locations after each round
-    with open(ppj("OUT_DATA_" + setup_name.upper(), "sample_{}_rep_{}.pickle".format(setup_name,rep_number)), "wb") as out_file:
+    with open(ppj("OUT_DATA_" + setup_name.upper(), "sample_{}_n={}_rep_{}.pickle".format(setup_name, n, rep_number)), "wb") as out_file:
         pickle.dump(data, out_file)
 
